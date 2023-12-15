457162d8ceb17075d961f5e131268c3a4aaa83d7
macedonsky777 <31594772+macedonsky777@users.noreply.github.com>
2023-12-15 21:55:28 +0200

    Update tests.py

--- tests/tests.py
+++ tests/tests.py
@@ -1,31 +1,21 @@
 import unittest
-import coverage  # Add this import
+import sys
+import os
 
-# Start code coverage measurement
-cov = coverage.Coverage()
-cov.start()
+# Add the path to the src directory
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../src")))
 
 from shop import Shop
 
 class TestShop(unittest.TestCase):
     def setUp(self):
         # Setup code that should be run before each test method
-        self.shop_manage = Shop(dbhost='localhost', dbuser='root', dbpass='qwerty1234', dbname='Shops', dbport='3306')
+        self.shop_manage = Shop(dbhost='mysql', dbuser='root', dbpass='qwerty1234', dbname='Shops', dbport='3306')
 
     def test_shop_connection(self):
         self.shop_manage.test_shop()
-        print("Woohoo!")
         # Add assertions based on the expected behavior of your test
 
 if __name__ == "__main__":
     print("Wait, testing...")
-
-    # Discover and run the tests
     unittest.main()
-
-    # Stop code coverage measurement and generate the report
-    cov.stop()
-    cov.save()
-
-    # Optionally, print the coverage report
-    cov.report()
