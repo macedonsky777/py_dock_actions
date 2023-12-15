341866b2f48af9b1945ec6dc6de98d7863d95615
macedonsky777 <31594772+macedonsky777@users.noreply.github.com>
2023-12-15 20:49:00 +0200

    Update db.py

--- src/db.py
+++ src/db.py
@@ -1,5 +1,4 @@
 import mysql.connector
-import pymysql
 from pymysql import Error
 
 class Mysql():
@@ -10,64 +9,31 @@ class Mysql():
         self._dbname = dbname
         self._dbport = dbport
         self.mydb = mysql.connector.connect(
-            host = dbhost,
-            user = dbuser,
-            password = dbpass,
-            port = dbport
-                                            )
+            host=dbhost,
+            user=dbuser,
+            password=dbpass,
+            port=dbport
+        )
         self._mycursor = self.mydb.cursor()
         self._mycursor.execute(f"USE {dbname}")
-        
-        max_retries = 30
-        retries = 0
-        
-        while True:
-            try:
-                self.mydb = mysql.connector.connect(
-                    dbhost=dbhost,
-                    dbuser=dbuser,
-                    dbpass=dbpass
-                )
-
-                print("MySQL is ready.")
-                break
-            except mysql.connector.Error as err:
-                
-                print(f"Waiting for MySQL... ({err})")
-                time.sleep(1)
-                retries += 1
-
-                if retries >= max_retries:
-                    print("Unable to connect to MySQL. Exiting.")
-                    exit(1)
-
 
     def create_db(self, dbname):
-        self.dbname = dbname
         self._mycursor.execute("CREATE DATABASE " + dbname)
 
     def create_table(self, tablename):
-        self.tablename = tablename
         self._mycursor.execute(f"CREATE TABLE {tablename} (id INT AUTO_INCREMENT PRIMARY KEY, item VARCHAR(255), price INT NOT NULL)")
 
     def insert_in_table(self, tablename, val=("itemname", "itempricenum")):
-        self.tablename = tablename
-        self.val = val
         sql = f"INSERT INTO {tablename} (item, price) VALUES (%s, %s)"
         self._mycursor.execute(sql, val)
 
     def delete_from_table(self, tablename, delitemname):
-        self.tablename = tablename
-        self.delitemname = delitemname
         sql_del = f"DELETE FROM {tablename} WHERE item = %s"
         self._mycursor.execute(sql_del, (delitemname,))
 
     def drop_table(self, tablename):
-        self.tablename = tablename
         drop_table_sql = f"DROP TABLE {tablename}"
         self._mycursor.execute(drop_table_sql)
-
-        self._mycursor.execute(sql)
 
     def show_all(self):
         self._mycursor.execute("SHOW DATABASES")
@@ -78,7 +44,7 @@ class Mysql():
         try:
             self.mydb.ping(reconnect=True)
             print("Connected to the database!")
-            self.mydb.close()
         except Error as e:
             print(f"An error occurred: {e}")
-            self._mydb.close()
+        finally:
+            self.mydb.close()
