b3cdbdf208ba891e591d485ec63fd0b1d59b501a
macedonsky777 <31594772+macedonsky777@users.noreply.github.com>
2023-12-15 23:20:05 +0200

    Update shop.py

--- src/shop.py
+++ src/shop.py
@@ -1,48 +1,33 @@
-from db import Mysql
-import time
+from mysql_connector import Mysql
 
-class Shop(Mysql):
-    def __init__(self, dbhost, dbuser, dbpass, dbname, dbport):
-        super().__init__(dbhost, dbuser, dbpass, dbname, dbport)
-        
-        self._mydb = self.mydb
-        self._mycursor = self._mydb.cursor()
 
-    def create_main_db(self, db_name):
-        self.create_db(dbname=db_name)
+class PetShop(Mysql):
+    def create_shop(self):
+        query = "CREATE DATABASE IF NOT EXISTS shop"
+        self.query(query)
+        query = "USE shop"
+        self.query(query)
+        query = "CREATE TABLE IF NOT EXISTS pets (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255), price INT)"
+        self.query(query)
 
-    def create_shop(self, shop_name):
-        self.create_table(tablename=shop_name)
-        print(f"Creating a shop with table named {shop_name}")
+    def add_item(self, name, price):
+        query = f"INSERT INTO pets (name, price) VALUES ('{name}', {price});"
+        self.query(query)
+        query = f"SELECT id, name FROM pets WHERE name='{name}'"
+        res = self.query(query)
+        ids = []
+        for id_name in res:
+            if id_name[1] == name:
+                ids.append(id_name[0])
+        return ids
 
-    def add_item(self, shop_name, item_name, item_price):
-        self.insert_in_table(tablename=shop_name, val=(item_name, item_price))
-        print(f"Adding item to {shop_name}: {item_name} priced at {item_price}")
+    def delete_item(self, name):
+        query = f"DELETE FROM pets WHERE name='{name}';"
+        return self.query(query)
 
-    def delete_item(self, shop_name, del_item_name):
-        self.delete_from_table(tablename=shop_name, delitemname=del_item_name) 
-        print(f"Deleting {del_item_name} from {shop_name} was successfull.")
-
-    def delete_shop(self, shop_name):
-        self.drop_table(tablename=shop_name)
-        print(f"Deleting {shop_name} was successfull.")
-
-    def show_all_staff(self):
-        databases = self.show_all()
-        print(databases)
-
-    def test_shop(self):
-        self.test_connection()
-
-#Usage
-shop_manage = Shop(dbhost='db', dbuser='macdev1', dbpass='123321', dbname='Shops', dbport='3306')
-
-shop_manage.test_shop()
-#shop_manage.create_main_db("Shops")
-#shop_manage.create_shop("Spooky_stuff")
-#shop_manage.add_item("Spooky_stuff", "putin_mask", 1)
-#shop_manage.show_all_staff("Spooky_stuff")
-#shop_manage.delete_item("Spooky_stuff", "putin_mask")
-#shop_manage.show_all_staff("Spooky_stuff")
-#shop_manage.delete_shop("Spooky_stuff")
-#shop_manage.show_all_staff()
+    def delete_item_by_id(self, id):
+        query = f"DELETE FROM pets WHERE id='{id}';"
+        return self.query(query)
+    
+    def delete_shop(self):
+        self.query("DROP TABLE pets")
