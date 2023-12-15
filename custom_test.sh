53fe12ec6a945683fafb24446865ab8fcabbcd40
O.Azarov <yano.motoharu.kikuchi@gmail.com>
2023-12-15 12:31:36 +0200

    hope it works

---
+++ custom_test.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+product=$1
+price=$2
+
+
+echo "Product: $product"
+echo "Price: $price"
---
+++ tests/tests.py
@@ -0,0 +1,22 @@
+import unittest
+from shop import Shop
+
+
+class TestMysql(unittest.TestCase):
+    def setUp(self):
+        self.shop = Shop("mysql", "root", "qwerty1234")
+        print("Nice!")
+    
+    def test_create_item(self):
+        self.shop.create_shop()
+        res = self.shop.add_item("test pet1", 20)
+        print(f"RES: {res}")
+        self.assertTrue(len(res) > 0)
+    
+    def tearDown(self):
+        self.shop.delete_shop()
+
+
+if __name__ == "__main__":
+    print("Wait, testing...")
+    unittest.main()
