03de4e958502a8e30f21c46a24d5b49f9606dba2
macedonsky777 <31594772+macedonsky777@users.noreply.github.com>
2023-12-15 23:18:59 +0200

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,15 +1,14 @@
-from shop import Shop
+from shop import PetShop
 import sys
-#shop_manage = Shop(dbhost='localhost', dbuser='root', dbpass='qwerty123', dbname='Shops', dbport='3306')
-#shop_manage.test_shop()
+
+
 host = sys.argv[1]
 user = sys.argv[2]
 password = sys.argv[3]
-#
-shop = Shop(host, user, password)
-shop.create_shop()
-ids = shop.add_item("Pikachu", 777)
+
+petshop = PetShop(host, user, password)
+petshop.create_shop()
+ids = petshop.add_item("Mikky mouse", 100)
 print(ids)
 for id in ids:
-    res = shop.delete_item_by_id(id)
-    
+    res = petshop.delete_item_by_id(id)
