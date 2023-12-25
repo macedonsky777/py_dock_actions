f67a555e8d64d0079e09ce077558c7d5b5c64116
macedonsky777 <31594772+macedonsky777@users.noreply.github.com>
2023-12-26 00:27:17 +0200

    Update main.py

--- src/main.py
+++ src/main.py
@@ -2,9 +2,9 @@ from shop import Shop
 import sys
 #shop_manage = Shop(dbhost='localhost', dbuser='root', dbpass='qwerty123', dbname='Shops', dbport='3306')
 #shop_manage.test_shop()
-host = sys.argv[1]
-user = sys.argv[2]
-password = sys.argv[3]
+#host = sys.argv[1]
+#user = sys.argv[2]
+#password = sys.argv[3]
 #
 shop = Shop(host="mysql", user="root", password="qwerty1234", dbname="popl", dbport=3306)
 shop.create_shop()
