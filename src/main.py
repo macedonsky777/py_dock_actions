5abc330a8347802486c6f44bbd152bf49a05c84b
macedonsky777 <31594772+macedonsky777@users.noreply.github.com>
2023-12-27 11:20:03 +0200

    Update main.py

--- src/main.py
+++ src/main.py
@@ -6,7 +6,7 @@ import sys
 #user = sys.argv[2]
 #password = sys.argv[3]
 #
-shop = Shop(work-mysql-1, dbuser="root", dbpass="qwerty1234", dbname="popl", dbport=3306)
+shop = Shop("mysql", dbuser="root", dbpass="qwerty1234", dbname="popl", dbport=3306)
 shop.create_shop()
 ids = shop.add_item("Pikachu", 777)
 print(ids)
